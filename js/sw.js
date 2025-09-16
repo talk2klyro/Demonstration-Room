573d640e03c55de2e5516c1d33e988a511940766
talk2klyro <talk2klyro@gmail.com>
2025-09-16 03:45:10 +0100

    Update sw.js

--- js/sw.js
+++ js/sw.js
@@ -1,39 +1,13 @@
-const CACHE_NAME = 'circle-cache-v1';
+const CACHE_NAME = "circle-cache-v1";
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/circles.html',
-  '/marketplace.html',
-  '/academics.html',
-  '/profile.html',
-  '/events.html',
-  '/css/style.css',
-  '/js/app.js',
-  '/js/helpers.js'
+  "index.html","circles.html","marketplace.html","academics.html",
+  "profile.html","events.html","css/style.css","js/app.js","js/carousel.js"
 ];
 
-// Install service worker
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
 });
 
-// Activate
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(key => key !== CACHE_NAME)
-        .map(key => caches.delete(key))
-      )
-    )
-  );
-});
-
-// Fetch from cache first
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(resp => resp || fetch(event.request))
-  );
+self.addEventListener("fetch", e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
